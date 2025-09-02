08b482b24b9840fe6c2ab0f7654a6d1927dd4687
SrKrakemon <efren.a.rm@gmail.com>
2025-09-02 11:52:04 -0600

    Cambios en .gitignore, Se creo el archivo estructura.sh

---
+++ estructura.sh
@@ -0,0 +1,17 @@
+  #! /bin/bash
+
+  # Genera estructura de proyecto 
+  
+  mkdir DEV 
+  mkdir -p DEV/dog-api/
+  mkdir -p DEV/dog-api/config/
+  mkdir -p DEV/dog-api/templates/
+  
+  touch DEV/dog-api/config/api-key.txt
+  touch DEV/dog-api/templates/index.html
+  
+
+  
+
+  
+
